358dd1387085d3c4b43122304c264c772c2cf3e3
Luka Milivojević <milivojevicl350@gmail.com>
2022-04-01 12:32:28 +0200

    soon to be request

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 from flask import Flask,jsonify,request,render_template
-from psycopg2 import connection
+#from psycopg2 import connect
+import psycopg2
 
 import os
 
 url = os.environ.get('DATABASE_URL')
+connection = psycopg2.connect(url)
+app = Flask(__name__)
+CREATE_TABLE = """CREATE TABLE IF NOT EXISTS rooms (
+                id SERIAL PRIMARY_KEY,
+                name TEXT
+            """
+
+
+"""
+{
+  "name": "Room name",
+  "temperatures": [
+    {"date": "2022-03-10", "temperature": 13.4},
+    {"date": "2022-03-09", "temperature": 14.4},
+    {"date": "2022-03-08", "temperature": 17.4},
+    {"date": "2022-03-07", "temperature": 13.4},
+    {"date": "2022-03-06", "temperature": 13.4},
+    {"date": "2022-03-05", "temperature": 16.4},
+    {"date": "2022-03-04", "temperature": 13.4},
+  ],
+  "average": 15.7
+}
+"""
+
+# POST /api/room
+@app.route('/api/room',methods=['POST'])
+def create_room():
+    data = request.get_json()
+    
+    return {"id": 2, "message": "Room {name} created."}
