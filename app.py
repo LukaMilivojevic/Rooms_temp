572d0ed62f51154e5e12c2513a935ff64c73f013
Luka Milivojević <milivojevicl350@gmail.com>
2022-04-06 12:58:03 +0200

    implementing first endpoint

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ import os
 url = os.environ.get('DATABASE_URL')
 connection = psycopg2.connect(url)
 app = Flask(__name__)
-CREATE_TABLE = "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT UNIQUE);"
+
+
+CREATE_ROOMS_TABLE = "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+CREATE_TEMPS_TABLE = "CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL, FOREIGN KEY(room_id) REFERENCES rooms(id));"
 
 INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
+INSERT_TEMP = "INSERT INTO temperatures (room_id, temperature) VALUES (%s, %s);"
 
 
 """
@@ -40,16 +44,25 @@ def create_room():
     name = data['name']
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(CREATE_TABLE)
-            try:
-                cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
-                room_id = cursor.fetchone()[0]
-                return {"id": room_id, "message": f"Room {name} created."}
-            except:
-                return {"message": f'Room named:"{name}" already exists'}
+            cursor.execute(CREATE_ROOMS_TABLE)
+            cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
+            room_id = cursor.fetchone()[0]
+    return {"id": room_id, "message": f"Room {name} created."}
+            
 
 
-#@app.route('/api/room',methods=['POST'])
+#{"temperature": 15.9, "room": 2}
+@app.route('/api/temperature',methods=['POST'])
+def add_temp():
+    data = request.get_json()
+    temperature = data['temperature']
+    room_id = data['room']
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_TEMPS_TABLE)
+            cursor.execute(INSERT_ROOM_RETURN_ID, (room_id, temperature))
+    return {"message": "Temperature added."}
+
 	
 
 if __name__ == "__main__":
