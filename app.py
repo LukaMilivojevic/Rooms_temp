c14f5d52d6bb55d33ad893b934aec394967a9cf8
Luka Milivojević <milivojevicl350@gmail.com>
2022-04-06 11:47:59 +0200

    implementing first endpoint

--- app.py
+++ app.py
@@ -7,43 +7,44 @@ import os
 url = os.environ.get('DATABASE_URL')
 connection = psycopg2.connect(url)
 app = Flask(__name__)
-CREATE_TABLE = """CREATE TABLE IF NOT EXISTS rooms (
-                id SERIAL PRIMARY KEY,
-                name TEXT);
-            """
+CREATE_TABLE = "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+
+INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
 
 
 """
 {
-  "name": "Room name",
-  "temperatures": [
-    {"date": "2022-03-10", "temperature": 13.4},
-    {"date": "2022-03-09", "temperature": 14.4},
-    {"date": "2022-03-08", "temperature": 17.4},
-    {"date": "2022-03-07", "temperature": 13.4},
-    {"date": "2022-03-06", "temperature": 13.4},
-    {"date": "2022-03-05", "temperature": 16.4},
-    {"date": "2022-03-04", "temperature": 13.4},
-  ],
-  "average": 15.7
+	"name": "Room name",
+	"temperatures": [
+		{"date": "2022-03-10", "temperature": 13.4},
+		{"date": "2022-03-09", "temperature": 14.4},
+		{"date": "2022-03-08", "temperature": 17.4},
+		{"date": "2022-03-07", "temperature": 13.4},
+		{"date": "2022-03-06", "temperature": 13.4},
+		{"date": "2022-03-05", "temperature": 16.4},
+		{"date": "2022-03-04", "temperature": 13.4},
+	],
+	"average": 15.7
 }
 """
 
 @app.route("/")
 def home_view():
-        return "<h1>Welcome to rooms temp control</h1>"
-  
+	return "<h1>Welcome to rooms temp control</h1>"
+	
 
-# POST /api/room {'name':room_name}
+# POST /api/room {'name':room_name}			
 @app.route('/api/room',methods=['POST'])
 def create_room():
     data = request.get_json()
     name = data['name']
     with connection:
-        cursor = connection.cursor()
-        cursor.execute(CREATE_TABLE)
-    return {"id": 2, "message": f"Room {name} created."}
-  
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_TABLE)
+            cursor.execute(INSERT_ROOM_RETURN_ID, name)
+            room_id = cursor.fetchone()[0]
+    return {"id": room_id, "message": f"Room {name} created."}
+	
 
 if __name__ == "__main__":
-        app.run()
+  	app.run()
