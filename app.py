291185e870f461b1e32b7b81284912a77680549a
Luka Milivojević <milivojevicl350@gmail.com>
2022-04-07 14:44:42 +0200

    saving dates to db

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,jsonify,request,render_template
+from datetime import datetime,timezone
 #from psycopg2 import connect
 import psycopg2
+import pytz
 
 import os
 
@@ -11,10 +13,10 @@ app = Flask(__name__)
 
 CREATE_ROOMS_TABLE = "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
 CREATE_TEMPS_TABLE = """"CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL, 
-                         date CURRENT_DATE, time CURRENT_TIME, FOREIGN KEY(room_id) REFERENCES rooms(id)) ON DELETE CASCADE;"""
+                         date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
 
 INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
-INSERT_TEMP = "INSERT INTO temperatures (room_id, temperature) VALUES (%s, %s);"
+INSERT_TEMP = "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
 
 
 """
@@ -58,10 +60,11 @@ def add_temp():
     data = request.get_json()
     temperature = data['temperature']
     room_id = data['room']
+    date = datetime.now(timezone.utc)
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(CREATE_TEMPS_TABLE)
-            cursor.execute(INSERT_TEMP, (room_id, temperature))
+            cursor.execute(INSERT_TEMP, (room_id, temperature, date))
     return {"message": "Temperature added."}
 
 
