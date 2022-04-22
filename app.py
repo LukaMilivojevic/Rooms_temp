9239cedf002eb435b2bc2da9af60591c85c56594
Nikola Milivojevic <milivojevicl350@gmail.com>
2022-04-22 12:15:36 +0200

    shortened queries

--- app.py
+++ app.py
@@ -10,19 +10,20 @@ CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER
 INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
 INSERT_TEMP = "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
 
-ROOM_AVG = """SELECT DATE(temperatures.date), rooms.name, AVG(temperatures.temperature) as average
-                       FROM rooms JOIN temperatures ON rooms.id = temperatures.room_id WHERE rooms.id = (%s) 
-                       GROUP BY DATE(temperatures.date), rooms.name"""
-ROOM_ALL_TIME_AVG = f"""SELECT name, COUNT(date) as number_of_days, AVG(average) as average_temp FROM 
-                       ({ROOM_AVG}) as days GROUP BY name;"""
+ROOM_NAME = """SELECT name FROM rooms WHERE id=(%s)"""
 
-MAX_DATE = """SELECT MAX(DATE(date))-(%s) FROM temperatures"""
-ROOM_TERM = f"""SELECT name, average, date FROM
-                (SELECT DATE(temperatures.date) as date, rooms.name, AVG(temperatures.temperature) as average
-                FROM rooms JOIN temperatures ON rooms.id = temperatures.room_id 
-                GROUP BY DATE(temperatures.date), rooms.name, rooms.id
-                HAVING rooms.id = (%s) AND DATE(temperatures.date) > ({MAX_DATE})) as days;
-            """ 
+# this query can be separated in 2 queries. one taking the average for all temperatures and other one counting
+# number of days separately
+ROOM_ALL_TIME_AVG = """SELECT AVG(average), COUNT(date) FROM
+(SELECT AVG(temperature) as average, DATE(date) FROM temperatures WHERE room_id = (%s) GROUP BY DATE(date)) as daily"""
+
+
+ROOM_TERM = """SELECT DATE(temperatures.date) as reading_date,
+AVG(temperatures.temperature)
+FROM temperatures
+WHERE temperatures.room_id = (%s)
+GROUP BY reading_date
+HAVING DATE(temperatures.date) > (SELECT MAX(DATE(temperatures.date))-(%s) FROM temperatures);"""
 
 DATE_AVG = """SELECT DATE(temperatures.date), AVG(temperatures.temperature) as average
               FROM rooms JOIN temperatures ON rooms.id = temperatures.room_id
@@ -70,9 +71,8 @@ def add_temp():
     return {"message": "Temperature added."}
 
 
-@app.route('/api/room/<string:id>')
-def get_room_all(id):
-    room_id = int(id)
+@app.route('/api/room/<int:room_id>')
+def get_room_all(room_id):
     args = request.args
     term = args.get("term")
     if term is not None:
@@ -80,21 +80,23 @@ def get_room_all(id):
     else:        
         with connection:
             with connection.cursor() as cursor:
+                cursor.execute(ROOM_NAME, (room_id,))
+                name = cursor.fetchone()
                 cursor.execute(ROOM_ALL_TIME_AVG, (room_id,))
-                row = cursor.fetchone()
-        return {"name": row[0], "average": round(row[2], 2), "days": row[1]}
+                days_temps = cursor.fetchone()
+        return {"name": name, "average": round(days_temps[0], 2), "days": days_temps[1]}
 
 
-def get_room_term(id, term):
-    room_id = int(id)
+def get_room_term(room_id, term):
     terms = {"week": 7, "month": 30}
     with connection:
         with connection.cursor() as cursor:
+            cursor.execute(ROOM_NAME, (room_id,))
+            name = cursor.fetchone()
             cursor.execute(ROOM_TERM, (room_id,terms[term]))
-            row = cursor.fetchall()
-    dates_temperatures = [{"date": element[2], "temperature": round(element[1], 2)} for element in row]
-    average = sum([elem["temperature"] for elem in dates_temperatures])/len(dates_temperatures)
-    return {"name": row[0][0], "temperatures": dates_temperatures, "average": round(average,2)}
+            dates_temperatures = cursor.fetchall()
+    average = sum(day[1] for day in dates_temperatures)/len(dates_temperatures) 
+    return {"name": name, "temperatures": dates_temperatures, "average": round(average,2)}
     
 
 @app.route('/api/average')
