3b58a0dc8131646529a1cdac1eeb6964108a4fca
Jose Salvatierra <jslvtr@gmail.com>
2022-04-25 13:49:35 +0100

    ♻️ (Add new queries file) Split out the new queries Luka wrote onto another file
    This should make them a little bit easier to read and we can then compare the two files across SQL and Python code more easily.

--- app_new_queries.py
+++ app_new_queries.py
@@ -17,9 +17,8 @@ INSERT_TEMP = (
 ROOM_NAME = """SELECT name FROM rooms WHERE id=(%s)"""
 
 ROOM_NUMBER_OF_DAYS = """SELECT DISTINCT COUNT(DATE(date)) OVER () AS days FROM temperatures WHERE room_id = (%s) GROUP BY DATE(date);"""
-ROOM_ALL_TIME_AVG = (
-    """SELECT AVG(temperature) as average FROM temperatures WHERE room_id = (%s);"""
-)
+ROOM_ALL_TIME_AVG = """SELECT AVG(average), COUNT(date) FROM
+(SELECT AVG(temperature) as average, DATE(date) FROM temperatures WHERE room_id = (%s) GROUP BY DATE(date)) as daily"""
 
 
 ROOM_TERM = """SELECT DATE(temperatures.date) as reading_date,
@@ -29,9 +28,7 @@ WHERE temperatures.room_id = (%s)
 GROUP BY reading_date
 HAVING DATE(temperatures.date) > (SELECT MAX(DATE(temperatures.date))-(%s) FROM temperatures);"""
 
-# same for this query
-GLOBAL_NUMBER_OF_DAYS = """SELECT DISTINCT COUNT(DATE(date)) OVER () AS days FROM temperatures GROUP BY DATE(date);"""
-GLOBAL_AVG = """SELECT AVG(temperature) as average FROM temperatures;"""
+GLOBAL_AVG = """SELECT AVG(average), COUNT(date) FROM (SELECT AVG(temperature) as average, DATE(date) FROM temperatures GROUP BY DATE(date)) as daily"""
 
 
 url = os.environ.get("DATABASE_URL")
@@ -86,10 +83,8 @@ def get_room_all(room_id):
                 cursor.execute(ROOM_NAME, (room_id,))
                 name = cursor.fetchone()
                 cursor.execute(ROOM_ALL_TIME_AVG, (room_id,))
-                average = cursor.fetchone()[0]
-                cursor.execute(ROOM_NUMBER_OF_DAYS, (room_id,))
-                days = cursor.fetchone()
-        return {"name": name, "average": round(average, 2), "days": days}
+                days_temps = cursor.fetchone()
+        return {"name": name, "average": round(days_temps[0], 2), "days": days_temps[1]}
 
 
 def get_room_term(room_id, term):
@@ -113,10 +108,8 @@ def get_global_avg():
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(GLOBAL_AVG)
-            average = cursor.fetchone()[0]
-            cursor.execute(GLOBAL_NUMBER_OF_DAYS)
-            days = cursor.fetchone()
-    return {"average": round(average, 2), "days": days}
+            row = cursor.fetchone()
+    return {"average": round(row[0], 2), "days": row[1]}
 
 
 if __name__ == "__main__":
